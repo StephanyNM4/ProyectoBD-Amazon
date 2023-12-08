2f79e307dc1c1e023f9140678fd4d7b51656a50e
StephanyNM4 <stephanymatamoros4@gmail.com>
2023-12-08 03:09:50 -0600

    rutas

--- index.js
+++ index.js
@@ -7,16 +7,24 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-//routers
-const consumidoresRouter = require('./routes/consumidor.router')
+
+const consumidoresRouter = require('./routes/consumidor.router');
+const pedidosRouter = require('./routes/pedido.router');
+const estadosRouter = require('./routes/estado.router');
+const direccionesRouter = require('./routes/direccion.router');
+const mensajesRouter = require('./routes/mensajes.router')
+const categoriasRouter = require('./routes/categorias.router')
+const productosRouter = require('./routes/productos.router')
+
 
 app.use('/consumidores', consumidoresRouter);
 app.use('/categorias', categoriasRouter);
 app.use('/productos', productosRouter);
-
+app.use('/mensajes', mensajesRouter);
 app.use('/pedidos', pedidosRouter);
 app.use('/estados', estadosRouter);
 app.use('/direcciones', direccionesRouter);
+
 
 app.get('/', (req, res) => {
     res.send("Servidor levantado correctamente");
